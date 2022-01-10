647acfb3e89cbec8b2b681781328452118af73a4
Royce551 <poohwie2005@gmail.com>
2022-01-09 20:51:17 -0600

    Sprinkle of xmldoc

---
+++ Squish/Interop/IEnvironment.cs
@@ -0,0 +1,14 @@
+﻿namespace Squish.Interop;
+
+public interface IEnvironment
+{
+    /// <summary>
+    /// All running windows in the environment
+    /// </summary>
+    public List<IWindow> RunningWindows { get; }
+
+    /// <summary>
+    /// Fired when windows are added or removed, or when window data changes
+    /// </summary>
+    public event EventHandler WindowsUpdated;
+}
--- squish/Interop/IWindow.cs
+++ squish/Interop/IWindow.cs
@@ -2,9 +2,18 @@
 
 public interface IWindow
 {
+    /// <summary>
+    /// Title of the window - to be seen on the taskbar
+    /// </summary>
     public string Title { get; }
 
+    /// <summary>
+    /// Data for the icon of the app in some sort of format Avalonia can display
+    /// </summary>
     public byte[]? Icon { get; }
 
+    /// <summary>
+    /// Whether the window is focused
+    /// </summary>
     public bool IsFocused { get; set; }
 }
