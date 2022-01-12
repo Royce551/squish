fa0bd986226ad0ac6ca079e31f28885a2cbb43b5
Vrabbers <25323861+Vrabbers@users.noreply.github.com>
2022-01-12 19:12:40 -0300

    supress xlib error code messages

---
+++ squish/GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Performance", "CA1806:Do not ignore method results", 
+    Justification = "Xlib rarely actually returns an error code, often just returning 1 or 0, seemingly at random.", 
+    Scope = "namespaceanddescendants", Target = "N:Squish.Interop.X11")]
